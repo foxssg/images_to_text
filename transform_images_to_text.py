c5146aaf1eb019b29829fde81ca93811fcbc49a8
foxssg <30871856+foxssg@users.noreply.github.com>
2019-04-17 14:53:50 -0300

    Update transform_images_to_text.py

--- transform_images_to_text.py
+++ transform_images_to_text.py
@@ -10,27 +10,28 @@ from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
 
 # locate the directory where the files are
-user_folder = input('type the location where your image files are')
+user_folder = input('type the location where your image files are > ')
 os.chdir(user_folder)
 
 
 # loop through all image files
-user_ask_extension = input('type the extension that you want to convert to pdf, for example png')
+user_ask_extension = input('type the extension that you want to convert to pdf, for example png > ')
+print('Working...')
 folders = glob.glob(user_folder)
 imagenames_list = []
 for folder in folders:
-    for f in sorted(glob.glob(folder+'/*.'+user_ask_extension)):
+    for f in sorted(glob.glob(folder+'/*.'+user_ask_extension.lower())):
         imagenames_list.append(f)
 
 read_images = []
-print(imagenames_list)
+#print(imagenames_list)
 
 # extract text from the image files
 for image in imagenames_list:
     Image.open(image)
     read_images.append(pytesseract.image_to_string(image, lang='eng'))
-    print(image)
-    print(read_images)
+    #print(image)
+    #print(read_images)
 str1 = ''.join(read_images)
 
 # creating and saving text to PDF
@@ -39,7 +40,7 @@ styleN = styles['Normal']
 styleH = styles['Heading1']
 story = []
 
-user_pdf_name = input('type a name for your PDF file')
+user_pdf_name = input('type a name for your PDF file > ')
 pdf_name = (user_pdf_name + '.pdf')
 doc = SimpleDocTemplate(
     pdf_name,
